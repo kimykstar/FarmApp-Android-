de0335b8232824ee88384bcfd21c59588e0e59fe
Younggwan <kimyogun78@naver.com>
2023-05-13 22:57:43 +0900

    카메라 열기

--- app/src/main/java/com/example/farm/MainActivity.java
+++ app/src/main/java/com/example/farm/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -36,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
             }
         });
 
