8f7ac00b81f5d56592e6f69ca9581e3a410c6d3b
김영관 <kimyogun78@naver.com>
2023-05-19 18:41:14 +0900

    commit

--- app/src/main/java/com/example/farm/MainActivity.java
+++ app/src/main/java/com/example/farm/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
+
             }
         });
 
