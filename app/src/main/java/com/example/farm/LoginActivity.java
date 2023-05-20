185dc7252127e7b1aba29da9b0e88182b6093551
김영관 <kimyogun78@naver.com>
2023-05-21 00:36:19 +0900

    login기능

--- app/src/main/java/com/example/farm/LoginActivity.java
+++ app/src/main/java/com/example/farm/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.example.farm;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.ExecutionException;
+
 public class LoginActivity extends AppCompatActivity {
     Button loginBtn;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +25,27 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         EditText idText = findViewById(R.id.id);
         EditText pwText = findViewById(R.id.pw);
-
+        Intent intent = new Intent(this, MainActivity.class);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginActivity activity = new loginActivity();
                 String id = idText.getText().toString();
                 String pw = pwText.getText().toString();
-                activity.execute(id, pw);
+                String result = null;
+                try {
+                    result = activity.execute(id, pw).get();
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Log.i("main : ", result);
+                if(result.equals("true")){
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this, "아이디 혹은 비밀번호를 확인하세요", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
@@ -37,10 +57,9 @@ public class LoginActivity extends AppCompatActivity {
             try{
                 String id = arg[0];
                 String pw = arg[1];
-                HttpConnection conn = new HttpConnection("http://10.0.2.2:8081/login");
+                HttpConnection conn = new HttpConnection("http://192.168.35.73:8081/login");
                 conn.setHeader(1000, "POST", true, true);
                 String message = String.format("%s %s", id, pw);
-                Log.i("data -->", message);
                 conn.writeData(message);
                 result = conn.readData();
                 Log.i("message", result);
