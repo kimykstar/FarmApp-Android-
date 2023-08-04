93ceec6520bd00a5ecd3c67775aa2a6810ea6879
김영관 <kimyogun78@naver.com>
2023-08-04 20:17:08 +0900

    modify : Chage Server ip address

--- app/src/main/java/com/example/farm/VideoActivity.java
+++ app/src/main/java/com/example/farm/VideoActivity.java
@@ -1,17 +1,24 @@
 package com.example.farm;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.Preview;
 import androidx.camera.core.VideoCapture;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 
 import com.google.common.util.concurrent.ListenableFuture;
@@ -29,55 +36,65 @@ public class VideoActivity extends AppCompatActivity {
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_layout);
 
-
         video = findViewById(R.id.viewFinder);
 
-        // ProcessCameraProvider는 카메라 제공자
+        // 카메라 권한 설정이 되지 않은 경우
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        } else {
+            setupCamera();
+        }
+    }
+
+    private void setupCamera() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                startCameraX(cameraProvider);
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
+                bindPreview(cameraProvider);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+        }, ContextCompat.getMainExecutor(this));
 
-        }, getExecutor());
-
+        video.setImplementationMode(PreviewView.ImplementationMode.COMPATIBLE);
+        video.setScaleType(PreviewView.ScaleType.FIT_CENTER);
     }
 
-    private Executor getExecutor() {
-        return ContextCompat.getMainExecutor(this);
-    }
-
-    @SuppressLint("RestrictedApi")
-    private void startCameraX(ProcessCameraProvider cameraProvider) {
-
-        cameraProvider.unbindAll();
+    // Preview위젯에 cameraprovider를 bind한다.
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
 
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
-        Preview preview = new Preview.Builder().build();
-
         preview.setSurfaceProvider(video.getSurfaceProvider());
 
-        imageCapture = new ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .build();
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
 
-        videoCapture = new VideoCapture.Builder()
-                .setVideoFrameRate(30)
-                .build();
-
-        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, videoCapture);
+    // requestPermission메소드를 실행하여 Permission요청 시 자동으로 동작하는 함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 부여되었을 때
+                setupCamera();
+            } else {
+                // 권한이 거부되었을 때
+                Toast.makeText(this, "카메라 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+                finish(); // 앱 종료 또는 다른 조치를 취할 수 있습니다.
+            }
+        }
     }
 }
